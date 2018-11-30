78806ff97fa1952be61ec1cc1d77412747f97521
Anthony Morast <anthonymorast33@hotmail.com>
2018-11-29 20:58:50 -0700

    working CNN and result files

--- ml_algs/cnn.py
+++ ml_algs/cnn.py
@@ -1,4 +1,4 @@
-from keras.models import Sequential
+from keras.models import Sequential, load_model
 from keras.layers import Dense, Conv2D, Flatten, MaxPooling2D
 from keras.utils import plot_model
 from keras.callbacks import ModelCheckpoint, TensorBoard
@@ -7,6 +7,7 @@ from keras.callbacks import ModelCheckpoint, TensorBoard
     Returns the model with time added in the ANN
 """
 def get_model1():
+    # https://stackoverflow.com/questions/47818968/adding-an-additional-value-to-a-convolutional-neural-network-input
     model = Sequential()
     # input_shape=(28, 28, 1) = width, height, channels[1=greyscale, 3=rgb]
     # kernel_size = size of kernel, 3=3x3
@@ -33,12 +34,23 @@ def get_model1():
 def get_model2(input_size=(160,350,3)):
     model = Sequential()
 
+    # was good....
     model.add(Conv2D(128, kernel_size=5, activation='relu', input_shape=input_size))
     model.add(Conv2D(128, kernel_size=3, activation='relu'))
     model.add(MaxPooling2D(pool_size=(3,3)))
     model.add(Conv2D(64, kernel_size=3, activation='relu'))
     model.add(Conv2D(64, kernel_size=3, activation='relu'))
     model.add(MaxPooling2D(pool_size=(4,4)))
+
+    # model.add(Conv2D(128, kernel_size=5, activation='relu', input_shape=input_size))
+    # model.add(Conv2D(128, kernel_size=3, activation='relu'))
+    # model.add(MaxPooling2D(pool_size=(5,5), dim_ordering='th'))
+    # model.add(Conv2D(128, kernel_size=3, activation='relu'))
+    # model.add(Conv2D(128, kernel_size=3, activation='relu'))
+    # model.add(MaxPooling2D(pool_size=(4,4), dim_ordering='th'))
+    # model.add(Conv2D(64, kernel_size=3, activation='relu'))
+    # model.add(Conv2D(64, kernel_size=3, activation='relu'))
+    # model.add(Conv2D(64, kernel_size=3, activation='relu'))
     model.add(Flatten())
     for _ in range(9):
         model.add(Dense(10, activation='relu'))
--- ml_algs/create_csv.py
+++ ml_algs/create_csv.py
@@ -2,13 +2,8 @@ import os
 import csv
 
 if __name__ == '__main__':
-<<<<<<< HEAD
     test_dir = 'test_bw'
     train_dir = 'train_bw'
-=======
-    test_dir = 'test'
-    train_dir = 'train'
->>>>>>> 92ad549a63fd9083d85972a6812eacf9cb68cdd5
 
     train_files = os.listdir(os.path.join('.', train_dir))
     test_files = os.listdir(os.path.join('.', test_dir))
@@ -22,11 +17,7 @@ if __name__ == '__main__':
         timestep = f[(f.index('_')+1):-4]
         rows.append([path, timestep, re])
 
-<<<<<<< HEAD
     with open('train_bw.csv', 'w') as f:
-=======
-    with open('train.csv', 'w') as f:
->>>>>>> 92ad549a63fd9083d85972a6812eacf9cb68cdd5
         writer = csv.writer(f)
         writer.writerows(rows)
 
@@ -39,10 +30,6 @@ if __name__ == '__main__':
         timestep = f[(f.index('_')+1):-4]
         rows.append([path, timestep, re])
 
-<<<<<<< HEAD
     with open('test_bw.csv', 'w') as f:
-=======
-    with open('test.csv', 'w') as f:
->>>>>>> 92ad549a63fd9083d85972a6812eacf9cb68cdd5
         writer = csv.writer(f)
         writer.writerows(rows)
