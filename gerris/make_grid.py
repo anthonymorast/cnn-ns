f825ee53990097f38ac0d0d1d682521dbaf04c71
Anthony Morast <anthonymorast33@hotmail.com>
2019-01-07 18:13:58 -0700

    updated gerris geometry and gitignore

--- gerris/make_grid.py
+++ gerris/make_grid.py
@@ -1,11 +1,20 @@
 import numpy as np
 
+<<<<<<< HEAD
+xmin = -2.5
+xmax = 4.5
+ymin = -2.5
+ymax = 2.5
+
+multiplier = 50     # determines fidelity of data 100 -> steps 0.01, 0.02, 0.03, etc.
+=======
 xmin = -0.5
 xmax = 3.5
 ymin = -0.5
 ymax = 1.5
 
 multiplier = 20     # determines fidelity of data 100 -> steps 0.01, 0.02, 0.03, etc.
+>>>>>>> b38b8a80b69e6aa3fea57b7b2e9ef3d61fe0deec
 
 ysteps = abs(ymin-ymax)*multiplier + 1
 xsteps = abs(xmin-xmax)*multiplier + 1
